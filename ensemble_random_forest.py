01902818f362b28c67b3c138de94a4bfda6b751e
Soojung <hong.soojung@gmail.com>
2018-05-07 13:56:04 +0200

    exercise code

--- ensemble_random_forest.py
+++ ensemble_random_forest.py
@@ -333,10 +333,17 @@ y3 = y2 - tree_reg2.predict(X)
 tree_reg3 = DecisionTreeRegressor(max_depth=2, random_state=42)
 tree_reg3.fit(X, y3)
 
+"""
+# exercise : sum() and elements in array
 X_new = np.array([[0.8]])
+X_new
+tree_reg1.predict(X_new)
+tree_reg2.predict(X_new)
+tree_reg3.predict(X_new)
+sum(tree.predict(X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
 y_pred = sum(tree.predict(X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
 y_pred
-
+"""
 def plot_predictions(regressors, X, y, axes, label=None, style="r-", data_style="b.", data_label=None):
     x1 = np.linspace(axes[0], axes[1], 500)
     y_pred = sum(regressor.predict(x1.reshape(-1,1)) for regressor in regressors)
@@ -349,4 +356,100 @@ def plot_predictions(regressors, X, y, axes, label=None, style="r-", data_style=
 plt.subplot(321)
 plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h_1(x_1)$", style="g-", data_label="Training set")
 plt.ylabel("$y$", fontsize=16, rotation=0)
-plt.title("Residuals and tree predictions", fontsize=16)    
+plt.title("Residuals and tree predictions", fontsize=16)  
+
+plt.subplot(322)
+plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1)$", data_label="Training set")
+plt.ylabel("$y$", fontsize=16, rotation=0)
+plt.title("Ensemble predictions", fontsize=16)
+
+plt.subplot(323)
+plot_predictions([tree_reg2], X, y2, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_2(x_1)$", style="g-", data_style="k+", data_label="Residuals")
+plt.ylabel("$y - h_1(x_1)$", fontsize=16)
+
+plt.subplot(324)
+plot_predictions([tree_reg1, tree_reg2], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1) + h_2(x_1)$")
+plt.ylabel("$y$", fontsize=16, rotation=0)
+
+plt.subplot(325)
+plot_predictions([tree_reg3], X, y3, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_3(x_1)$", style="g-", data_style="k+")
+plt.ylabel("$y - h_1(x_1) - h_2(x_1)$", fontsize=16)
+plt.xlabel("$x_1$", fontsize=16)
+
+plt.subplot(326)
+plot_predictions([tree_reg1, tree_reg2, tree_reg3], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1) + h_2(x_1) + h_3(x_1)$")
+plt.xlabel("$x_1$", fontsize=16)
+plt.ylabel("$y$", fontsize=16, rotation=0)
+
+
+from sklearn.ensemble import GradientBoostingRegressor
+gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=3, learning_rate=1.0, random_state=42)
+gbrt.fit(X,y)
+plt.figure(figsize=(11,4))
+plt.subplot(121)
+plot_predictions([gbrt], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="Ensemble predictions")
+plt.title("learning_rate={}, n_estimator={}".format(gbrt.learning_rate, gbrt.n_estimators), fontsize=14)
+
+gbrt_slow = GradientBoostingRegressor(max_depth=2, n_estimators=200, learning_rate=0.1, random_state=42)
+gbrt_slow.fit(X,y)
+
+plt.subplot(122)
+plot_predictions([gbrt_slow], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="Ensemble predictions")
+plt.title("learning_rate={}, n_estimator={}".format(gbrt.learning_rate, gbrt.n_estimators), fontsize=14)
+
+#-----------------------------------------------------------------------
+# Gradient Boosting with early stopping using when the error become min
+#-----------------------------------------------------------------------
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=49)
+X_train
+y_train
+gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=120, random_state=42)
+gbrt.fit(X_train, y_train)
+errors = [mean_squared_error(y_val, y_pred) for y_pred in gbrt.staged_predict(X_val)]
+errors
+bst_n_estimators = np.argmin(errors)
+bst_n_estimators
+gbrt_best = GradientBoostingRegressor(max_depth=2, n_estimators=bst_n_estimators, random_state=42)
+gbrt_best.fit(X_train, y_train)
+min_error = np.min(errors)
+min_error
+
+plt.figure(figsize=(11,4))
+plt.subplot(121)
+plt.plot(errors, "b.-")
+plt.plot([bst_n_estimators, bst_n_estimators], [0, min_error], "k--")
+plt.plot([0,120], [min_error, min_error], "k--")
+plt.plot(bst_n_estimators, min_error, "ko")
+plt.text(bst_n_estimators, min_error*1.2, "Minimum", ha="center", fontsize=14)
+plt.axis([0, 120, 0, 0.01])
+plt.xlabel("Number of trees")
+plt.title("Validation error", fontsize=14)
+
+plt.subplot(122)
+plot_predictions([gbrt_best], X, y, axes=[-0.5, 0.5, -0.1, 0.8])
+plt.title("Best model (%d trees)" % bst_n_estimators, fontsize=14)
+
+gbrt = GradientBoostingRegressor(max_depth=2, warm_start=True, random_state=42)
+min_val_error = float("inf")
+error_going_up = 0 
+for n_estimators in range(1, 120):
+    gbrt.n_estimators = n_estimators
+    gbrt.fit(X_train, y_train)
+    y_pred = gbrt.predict(X_val)
+    val_error = mean_squared_error(y_val, y_pred)
+    if val_error < min_val_error:
+        min_val_error = val_error
+        error_going_up = 0
+    else:
+        error_going_up += 1
+        if error_going_up == 5:
+            break #early stopping
+
+
+print(gbrt.n_estimators) #in this case, number of estimator, i.e. number of decision tree is 61 
+
+
