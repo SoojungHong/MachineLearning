1053faf86947410ef2b0dc001de56abfe8d8acb9
Soojung <hong.soojung@gmail.com>
2018-05-07 16:20:25 +0200

    adding TensorFlow exercise

---
+++ TensorFlow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  7 16:18:26 2018
+
+@author: soojunghong
+@reference: https://www.safaribooksonline.com/library/view/hands-on-machine-learning/9781491962282/ch09.html#tensorflow_chapter
+
+"""
+
+
