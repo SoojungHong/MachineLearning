71c4a5beb028171f27b860a99db9a9d527faf7ed
Soojung <hong.soojung@gmail.com>
2018-05-08 14:04:59 +0200

    initial TensorFlow

--- TensorFlow.py
+++ TensorFlow.py
@@ -7,5 +7,17 @@ Created on Mon May  7 16:18:26 2018
 @reference: https://www.safaribooksonline.com/library/view/hands-on-machine-learning/9781491962282/ch09.html#tensorflow_chapter
 
 """
+import tensorflow as tf
 
+hello = tf.constant("Hello, TensorFlow!")
+print(hello)
 
+x = tf.Variable(3, name="x")
+y = tf.Variable(4, name="y")
+f = x*x*y + y + 2
+
+sess = tf.Session()
+sess.run(x.initializer)
+sess.run(y.initializer)
+result = sess.run(f)
+print(result)
