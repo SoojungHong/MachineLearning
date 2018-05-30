4b4a5e21db5060791e2cb822159da43070aa320a
Soojung <hong.soojung@gmail.com>
2018-05-30 13:20:16 +0200

    gradient descent with tensorflow

--- TensorFlow.py
+++ TensorFlow.py
@@ -67,3 +67,64 @@ with tf.Session() as sess:
 #------------------------------------
 # Linear Regression with TensorFlow
 #------------------------------------
+import numpy as np
+import tensorflow as tf
+
+from sklearn.datasets import fetch_california_housing #worked after installing scipy
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+housing.data
+housing_data_plus_bias = np.c_[np.ones((m,1)), housing.data] #c_ is translates slice objects to concatenation along the second axis
+housing_data_plus_bias
+
+X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
+XT = tf.transpose(X)
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+
+with tf.Session() as sess:
+    theta_value = theta.eval()
+    
+    
+#-----------------------------------
+# Manually computing the gradients 
+#-----------------------------------
+from sklearn.preprocessing import StandardScaler 
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+scaled_housing_data_plus_bias
+print(scaled_housing_data_plus_bias.mean(axis=0))
+print(scaled_housing_data_plus_bias.mean(axis=1))
+print(scaled_housing_data_plus_bias.mean())
+print(scaled_housing_data_plus_bias.shape)
+
+tf.reset_default_graph()
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n+1, 1], -1.0, 1.0), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions") # y_prediction equals X*theta
+error = y_pred - y #error is difference between predicted value and actual value
+mse = tf.reduce_mean(tf.square(error), name="mse")
+gradients = 2/m * tf.matmul(tf.transpose(X), error)
+training_op = tf.assign(theta, theta - learning_rate * gradients)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init) 
+    
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE = ", mse.eval())
+        sess.run(training_op)
+    
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)    
