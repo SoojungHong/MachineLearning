c89bdc61f47b9c5242bdc58c62c2c8e62e4fb537
Soojung <hong.soojung@gmail.com>
2018-06-22 17:26:35 +0200

    singular value decomposition

--- singular_value_decomposition.py
+++ singular_value_decomposition.py
@@ -6,3 +6,14 @@ Created on Fri Jun 22 17:08:24 2018
 @author: soojunghong
 """
 
+from numpy import array
+from scipy.linalg import svd
+
+A = array([[1,2], [3,4], [5,6]])
+print(A)
+
+#singular value decomposition
+U, s, VT = svd(A)
+print(U)
+print(s)
+print(VT)
