697f5473bc8598bc9f3158a76dad1b3a62632abc
Soojung <hong.soojung@gmail.com>
2018-07-08 13:29:49 +0200

    Performance measure of models (esp. classifier)

---
+++ Model_Performance_Measure.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  8 13:27:17 2018
+
+@author: soojunghong
+
+@About : Performance evaluation (of Models - classifiers)
+"""
+
+
+
