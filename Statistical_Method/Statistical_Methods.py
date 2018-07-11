ae47180591a4c845ef3fa28bb61038c7a7cdf671
Soojung <hong.soojung@gmail.com>
2018-07-11 13:29:54 +0200

    init

---
+++ Statistical_Method/Statistical_Methods.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 11 13:18:25 2018
+
+@author: soojunghong
+
+@reference: https://machinelearningmastery.com/statistical-methods-in-an-applied-machine-learning-project/?utm_medium=email&utm_source=topic+optin&utm_campaign=awareness&utm_content=20180704+data+nl&mkt_tok=eyJpIjoiTlRFNVpEaGxaREF5WlRoaCIsInQiOiJEWGZVUCtCWmZKZXhQRktcLzFZWVdoRm9Kd1NYWmd4b09LY014akhsM1dcL1pUQW03QThCRU11bjZtWStFN1wvS25UYnRjaFQyM2liWEE2eldJTmNGSWlkT2w2QitsMFVwSGJ3VnltdVZ1eW44Rmx0TzdreW84aVVcL3FGRVVLRVR0WmcifQ%3D%3D
+"""
+
